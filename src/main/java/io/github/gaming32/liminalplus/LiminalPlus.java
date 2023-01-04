89f795fc93a35f7044d6484c9a857e4fca0b4376
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2023-01-04 16:36:56 -0600

    Initial commit

---
+++ src/main/java/io/github/gaming32/liminalplus/LiminalPlus.java
@@ -0,0 +1,10 @@
+package io.github.gaming32.liminalplus;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+
+public class LiminalPlus implements ModInitializer {
+    @Override
+    public void onInitialize(ModContainer mod) {
+    }
+}
