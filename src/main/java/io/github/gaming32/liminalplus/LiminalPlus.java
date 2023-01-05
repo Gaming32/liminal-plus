5f4487be10ff8464b971709d168a0e1e3b89bb0d
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2023-01-04 18:02:03 -0600

    automaticPaintingPortal

--- src/main/java/io/github/gaming32/liminalplus/LiminalPlus.java
+++ src/main/java/io/github/gaming32/liminalplus/LiminalPlus.java
@@ -2,8 +2,12 @@ package io.github.gaming32.liminalplus;
 
 import eu.midnightdust.lib.config.MidnightConfig;
 import io.github.gaming32.liminalplus.config.LiminalPlusConfig;
+import net.ludocrypt.corners.entity.DimensionalPaintingEntity;
+import net.minecraft.entity.decoration.painting.PaintingEntity;
+import net.minecraft.util.registry.Registry;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.quiltmc.qsl.entity_events.api.ServerEntityLoadEvents;
 
 public class LiminalPlus implements ModInitializer {
     public static final String MOD_ID = "liminal-plus";
@@ -11,5 +15,20 @@ public class LiminalPlus implements ModInitializer {
     @Override
     public void onInitialize(ModContainer mod) {
         MidnightConfig.init(MOD_ID, LiminalPlusConfig.class);
+
+        ServerEntityLoadEvents.AFTER_LOAD.register((entity, world) -> {
+            if (
+                LiminalPlusConfig.automaticPaintingPortal &&
+                    entity instanceof PaintingEntity painting &&
+                    !(entity instanceof DimensionalPaintingEntity) &&
+                    Registry.PAINTING_VARIANT.getId(painting.getVariant().value()).getNamespace().equals("corners")
+            ) {
+                final DimensionalPaintingEntity newEntity = DimensionalPaintingEntity.create(
+                    world, painting.getDecorationBlockPos(), painting.getHorizontalFacing(), painting.getVariant().value()
+                );
+                world.spawnEntity(newEntity);
+                entity.discard();
+            }
+        });
     }
 }
--- src/main/java/io/github/gaming32/liminalplus/config/LiminalPlusConfig.java
+++ src/main/java/io/github/gaming32/liminalplus/config/LiminalPlusConfig.java
@@ -1,4 +1,8 @@
 package io.github.gaming32.liminalplus.config;
 
+import eu.midnightdust.lib.config.MidnightConfig;
+
 public class LiminalPlusConfig {
+    @MidnightConfig.Entry
+    public static boolean automaticPaintingPortal = true;
 }
